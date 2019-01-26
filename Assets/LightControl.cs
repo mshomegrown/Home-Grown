39d611a781bf4e2a6b28f6a3bd08a4f056a80e52
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MacBook-Pro-2.local>
2019-01-26 11:45:55 -0500

    feat(MainScene): add movement to player and create demo room

---
+++ Assets/LightControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour {
+    float moveX;
+    float moveZ;
+    [SerializeField] float speed = 2f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveX = Input.GetAxis("Horizontal");
+        moveZ = Input.GetAxis("Vertical");
+        transform.position = transform.position + new Vector3(-moveX * speed, 0, -moveZ * speed) * Time.deltaTime;
+	}
+}
